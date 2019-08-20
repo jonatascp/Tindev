c4904f8f23b28ca21ea09b073a62dabd02010451
Jonatas <jonatas.pereira@pitang.com>
2019-08-20 14:36:59 -0300

    commit project week omnistack

---
+++ backend/src/routes.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const routes = express.Router()
+
+const DevController = require('./controllers/DevControllers')
+const LikeController = require('./controllers/LikeControllers')
+const DislikeController = require('./controllers/DislikeControllers')
+
+routes.post('/devs', DevController.store)
+routes.get('/devs', DevController.index)
+routes.post('/devs/:devId/likes', LikeController.store)
+routes.post('/devs/:devId/dislikes', DislikeController.store)
+
+module.exports = routes
